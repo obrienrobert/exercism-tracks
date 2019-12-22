f2b3a5f3b75d6c162b1048cbce47fc1d810b41be
obrienrobert <robrien@redhat.com>
2019-12-22 01:56:12 +0000

    Go track space age

---
+++ README.md
@@ -0,0 +1,2 @@
+# exercism-tracks
+[Exercism](https://exercism.io/) Bash and Go track solutions
--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -1,12 +1,29 @@
 package space
 
-import (
-	"fmt"
-)
+type Planet string
 
-func Age(seconds int, Planet string) {
+func Age(seconds float64, planet Planet) float64 {
 
-	for _, tc := range testCases {
-		fmt.Println(tc.planet)
+	earthYear := 31557600.0
+	planetYear := 1.0
+
+	switch planet {
+	case "Mercury":
+		planetYear = 0.2408467
+	case "Venus":
+		planetYear = 0.61519726
+	case "Mars":
+		planetYear = 1.8808158
+	case "Jupiter":
+		planetYear = 11.862615
+	case "Saturn":
+		planetYear = 29.447498
+	case "Uranus":
+		planetYear = 84.01684
+	case "Neptune":
+		planetYear = 164.79132
 	}
+
+	age := seconds / planetYear / earthYear
+	return age
 }
